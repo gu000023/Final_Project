62714f595c40a9130258b7cac6d1127cf8aabc9f
gu000023 <gu000023@algonquinlive.com>
2020-01-15 15:24:40 -0500

    Modified 3 layouts for Lab 2(v1.4 0115); Added snackbar with "undo" functionality and toast msg as required

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,13 +3,67 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
-        //comment
+        setContentView(R.layout.activity_main_relative);
+
+        //added requirements for lab 2
+        Button btn=findViewById(R.id.button2);//change to btn 3 4 if needed
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        CheckBox cb=(CheckBox) findViewById(R.id.checkBox);
+        Switch s=(Switch) findViewById(R.id.switch1);
+        //change cb 2 3, s 123 if needed
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(final CompoundButton cBtn, final boolean isChecked){
+                class UndoListener implements View.OnClickListener{
+                    public void onClick(View v){
+                        cBtn.setChecked(!isChecked);
+                    }
+                }
+                if(isChecked) {
+                    Snackbar.make(findViewById(R.id.rl), R.string.on, Snackbar.LENGTH_SHORT).setAction(R.string.undo, new UndoListener()).show();
+                }
+                else {
+                    Snackbar.make(findViewById(R.id.rl), R.string.off, Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(final CompoundButton cBtn, final boolean isChecked){
+                class UndoListener implements View.OnClickListener{
+                    public void onClick(View v){
+                        cBtn.setChecked(!isChecked);
+                    }
+                }
+                if(isChecked) {
+                    Snackbar.make(findViewById(R.id.rl), R.string.on, Snackbar.LENGTH_SHORT).setAction(R.string.undo, new UndoListener()).show();
+                }
+                else {
+                    Snackbar.make(findViewById(R.id.rl), R.string.off, Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
